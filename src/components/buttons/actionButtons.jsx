d8942e3cb5e633f58ac8241dbf9b5c71b9056dc7
Ari Hammond <ajhammond06@gmail.com>
2026-01-06 13:22:27 -0500

    Update Resume link in ActionButtons component

--- src/components/buttons/actionButtons.jsx
+++ src/components/buttons/actionButtons.jsx
@@ -2,7 +2,7 @@ function ActionButtons() {
     return (
         <div id="actionButtons">
             <button><a href="#" type="application.pdf">Academic CV</a></button>
-            <button><a href="https://drive.google.com/file/d/1T2Uuc4yv5YrstYMTzhYK3j5mQb2fljH2/view?usp=drive_link" type="application.pdf">Resume</a></button>
+            <button><a href="https://drive.google.com/file/d/1vbb6V143athxtXIOkp8I3C-Eo7zpsFoL/view?usp=sharing" type="application.pdf">Resume</a></button>
         </div>
     );
 }
